f76f45c45456457730da8fd6cbd69fc68075787f
malyvsen <5940672+malyvsen@users.noreply.github.com>
2020-10-07 21:37:59 +0200

    make text accessible from python

---
+++ tokipona/text.py
@@ -0,0 +1,36 @@
+import os
+
+
+
+with open(os.path.join(os.path.dirname(__file__), 'text.txt'), 'r') as file:
+    file_contents = file.read().strip()
+
+def sanitize(paragraph):
+    replace_with_spaces = '\n’\'":,;'
+    for symbol in replace_with_spaces:
+        paragraph = paragraph.replace(symbol, ' ')
+    return paragraph
+
+paragraphs = [
+    sanitize(paragraph)
+    for paragraph in file_contents.split('\n\n')
+]
+
+def split_sentences(paragraph):
+    sentence_endings = '.!?()[]'
+    for symbol in sentence_endings:
+        paragraph = paragraph.replace(symbol, '.')
+    return paragraph.split('.')
+
+def split_words(sentence):
+    return sentence.split()
+
+text = [
+    words
+    for words in [
+        split_words(sentence)
+        for paragraph in paragraphs
+        for sentence in split_sentences(paragraph)
+    ]
+    if words != []
+]
